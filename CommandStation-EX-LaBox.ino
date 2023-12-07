b429d85293b51fd400851265bde92eb4d2959927
BOBILLEChristophe <38000184+BOBILLEChristophe@users.noreply.github.com>
2023-12-07 19:00:55 +0100

    Update CommandStation-EX-LaBox.ino

--- CommandStation-EX-LaBox.ino
+++ CommandStation-EX-LaBox.ino
@@ -76,6 +76,11 @@ hmi boxHMI(&Wire);
 #endif
 //----------------------------------------------------------------------------
 
+//--------------------------- CAN messaging -------------------------------------
+#ifdef CAN
+#include "CanMsg.h"
+#endif
+//----------------------------------------------------------------------------
 
 void setup()
 {
@@ -95,7 +100,7 @@ void setup()
   ADCee::begin();
 
   DIAG(F("License GPLv3 fsf.org (c) Locoduino.org"));
-  DIAG(F("LaBox : 2.4.0"));
+  DIAG(F("LaBox : 2.4.1"));
 
   DISPLAY_START(
       // This block is still executed for DIAGS if display not in use
