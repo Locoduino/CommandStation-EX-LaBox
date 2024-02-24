230337a7093e7984ed2ae6bfd074b2b56a0299a8
BOBILLEChristophe <38000184+BOBILLEChristophe@users.noreply.github.com>
2024-02-24 09:51:21 -0500

    Update CanMsg.cpp

--- CanMsg.cpp
+++ CanMsg.cpp
@@ -11,17 +11,64 @@
   v 0.5.2 - 09/12/23
   v 0.5.3 - 10/12/23
   v 0.5.4 - 10/12/23 : Add POWERMODE::OVERLOAD
+  v 0.5.5 - 11/12/23 : Correction inversion :
+                       case 0xFE:
+                       TrackManager::setMainPower(frameIn.data[0] ? POWERMODE::OFF : POWERMODE::ON);
+  /*******************************************************************************************************
+  v 0.6.0 - 11/12/23 : Adoption d'un nouveau format de messages totalement incompatible avec les anciens
+  v 0.6.1 - 11/12/23 : Ajout du retour d'informations
+                       Ajouts de commandes dont la POM  case 0xF7:
+                                                        // WRITE CV on MAIN <w CAB CV VALUE>
+  v 6.0.2 - 12/12/23 : Ajout d'une méthode pour acquisition de hash (supprimé ensuite)
+                       La declaration des broches du CAN et de la vitesse ont ete deplcees lignes 184 à 186 de config.h
+  v 6.0.3 - 21/02/24 : Modification des identifiants de messages CAN
+
 */
 
 #include "CanMsg.h"
 
-gpio_num_t CanMsg::RxPin = GPIO_NUM_4;               // Optional, default Rx pin is GPIO_NUM_4
-gpio_num_t CanMsg::TxPin = GPIO_NUM_5;               // Optional, default Tx pin is GPIO_NUM_5
-uint32_t CanMsg::DESIRED_BIT_RATE = 1000UL * 1000UL; // 1 Mb/s;
+// - Config, voir lignes 184 à 186 de config.h
+gpio_num_t CanMsg::RxPin = CAN_RX;
+gpio_num_t CanMsg::TxPin = CAN_TX;
+uint32_t CanMsg::DESIRED_BIT_RATE = CAN_BITRATE;
+uint8_t CanMsg::thisId = 253;
+
+uint8_t CanMsg::getId() { return thisId; }
+
+QueueHandle_t xQueue; // Queue pour stocker les messages retour
+
+/************************************************************************
+Retour de LaBox pour les commandes de traction,la fonction emergency stop
+la fonction power on / power off et plus generalement par la suite, toutes
+les fonctions qui emettent une confirmation dans DCC-Ex
+*************************************************************************/
+
+void ackTask(void *pvParameters)
+{
+  BaseType_t status;
+  CANMessage frame;
+  //frame.ext = true;
+
+  for (;;)
+  {
+    status = xQueueReceive(xQueue, &frame, pdMS_TO_TICKS(100));
+    if (status == pdPASS)
+    {
+      frame.id &= ~ 0x18000000;
+      frame.id |= 3 << 27;               // Priorite 0, 1, 2 ou 3
+      frame.id &= ~ 0x7F800;
+      frame.id |= CanMsg::getId() << 11; // ID expediteur
+      frame.id &= ~ 0x04;
+      frame.id |= 1 << 2;                // Réponse
+      CanMsg::sendMsg(frame);
+    }
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
+}
 
 void CanMsg::begin()
 {
-  // debug.printf("[CanConfig %d] : Configure ESP32 CAN\n", __LINE__);
+  Serial.printf("[CanMsg %d] : Configure ESP32 CAN\n", __LINE__);
   ACAN_ESP32_Settings settings(DESIRED_BIT_RATE);
   settings.mRxPin = RxPin;
   settings.mTxPin = TxPin;
@@ -30,20 +77,22 @@ void CanMsg::begin()
 
   // with filter
   const ACAN_ESP32_Filter filter = ACAN_ESP32_Filter::singleExtendedFilter(
-      ACAN_ESP32_Filter::data, 0xF << 7, 0x1FFFF87F);
+      ACAN_ESP32_Filter::data, 0xF << 23, 0x187FFFFB);
   errorCode = ACAN_ESP32::can.begin(settings, filter);
 
   // without filter
   // errorCode = ACAN_ESP32::can.begin(settings);
-  // debug.printf("[CanConfig %d] : config without filter\n", __LINE__);
+  // Serial.printf("[CanMsg %d] : config without filter\n", __LINE__);
 
   if (errorCode == 0)
-    Serial.printf("[CanConfig %d] : configuration OK !\n", __LINE__);
+    Serial.printf("[CanMsg %d] : configuration OK !\n", __LINE__);
   else
   {
-    Serial.printf("[CanConfig %d] : configuration error 0x%x\n", __LINE__, errorCode);
+    Serial.printf("[CanMsg %d] : configuration error 0x%x\n", __LINE__, errorCode);
     return;
   }
+  xQueue = xQueueCreate(10, sizeof(CANMessage));
+  xTaskCreate(ackTask, "ackTask", 2 * 1024, NULL, 0, NULL);
 }
 
 /*--------------------------------------
@@ -55,41 +104,50 @@ void CanMsg::loop()
   CANMessage frameIn;
   if (ACAN_ESP32::can.receive(frameIn))
   {
-    const byte idSatExpediteur = (frameIn.id & 0x7F80000) >> 19; // ID expediteur
-    const byte fonction = (frameIn.id & 0x7F8) >> 3;
+    const uint8_t cmde = (frameIn.id & 0x7F80000) >> 19; // Commande
+    const uint8_t exped = (frameIn.id & 0x7F800) >> 11;  // Expéditeur
+    const uint8_t resp = (frameIn.id & 0x04) >> 2;       // Commande = 0 / Reponse = 1
 
-    Serial.printf("\n[CanMsg %d]------ Expediteur %d : Fonction 0x%0X\n", __LINE__, idSatExpediteur, fonction);
+    Serial.printf("\n[CanMsg %d]------ Expediteur %d : Commande 0x%0X\n\n", __LINE__, exped, cmde);
 
     if (frameIn.rtr) // Remote frame
       ACAN_ESP32::can.tryToSend(frameIn);
     else
     {
       uint16_t loco = 0;
-      if (fonction < 0xFE)
+      if (cmde < 0xFA)
         loco = (frameIn.data[0] << 8) + frameIn.data[1];
 
-      switch (fonction) // Fonction appelée
+      switch (cmde) // Fonction appelée
       {
       case 0xF0:
         DCC::setThrottle(loco, frameIn.data[2], frameIn.data[3]);
+        xQueueSendToBack(xQueue, &frameIn, 0);
         break;
       case 0xF1:
         DCC::setFn(loco, frameIn.data[2], frameIn.data[3]); // frame.data[2] = fonction, frame.data[3] : 'on' ou 'off'
         break;
+      case 0xF7:
+        // WRITE CV on MAIN <w CAB CV VALUE>
+        DCC::writeCVByteMain(loco, frameIn.data[2], frameIn.data[3]);
+        break;
       case 0xFE:
         TrackManager::setMainPower(frameIn.data[0] ? POWERMODE::ON : POWERMODE::OFF);
+        xQueueSendToBack(xQueue, &frameIn, 0);
         break;
       case 0xFF:
         DCC::setThrottle(0, 1, 1); // emergency stop
+        xQueueSendToBack(xQueue, &frameIn, 0);
         break;
       }
     }
   }
 
   /*--------------------------------------
-    Envois CAN
+    Envoi mesure de courant
     --------------------------------------*/
-
+// #define MESURE_COURANT
+#ifdef MESURE_COURANT
   static uint64_t millisRefreshData = 0;
   if (millis() - millisRefreshData > 1000)
   {
@@ -99,19 +157,20 @@ void CanMsg::loop()
       mainDriver = md;
     if (mainDriver == NULL || !mainDriver->canMeasureCurrent())
       return;
-      
+
     POWERMODE mode = TrackManager::getMainPower();
     if (mode == POWERMODE::ON)
     {
       uint16_t current = mainDriver->getCurrentRaw();
-      sendMsg(0, 0xAA, 0xAB, 0xFD, 1, (current & 0xFF00) >> 8, current & 0x00FF);
+      sendMsg(0, 0xFD, CanMsg::getId(), 0, 1, (current & 0xFF00) >> 8, current & 0x00FF);
     }
-    else if(mode == POWERMODE::OVERLOAD)
-      sendMsg(0, 0xAA, 0xAB, 0xFD, 2, 0, 0);
+    else if (mode == POWERMODE::OVERLOAD)
+      sendMsg(0, 0xFD, CanMsg::getId(), 0, 1, 2);
     else
-      sendMsg(0, 0xAA, 0xAB, 0xFD, 0, 0, 0);
+      sendMsg(0, 0xFD, CanMsg::getId(), 0, 1, 0);
     millisRefreshData = millis();
   }
+#endif
 }
 
 /*--------------------------------------
@@ -122,124 +181,63 @@ void CanMsg::sendMsg(CANMessage &frame)
 {
   if (0 == ACAN_ESP32::can.tryToSend(frame))
     Serial.printf("Echec envoi message CAN\n");
-  else
-    Serial.printf("Envoi fonction 0x%0X\n", (frame.id & 0x7F8) >> 3);
 }
 
-auto parseMsg = [](CANMessage &frame, byte priorite, byte idExp, byte idDes, byte fonct) -> CANMessage
+auto formatMsg = [](CANMessage &frame, byte prio, byte cmde, byte thisNodeId, byte destNodeId, byte resp) -> CANMessage
 {
-  frame.id |= priorite << 27; // Priorite 0, 1 ou 2
-  frame.id |= idExp << 19;    // ID expediteur
-  frame.id |= idDes << 11;    // Hash
-  frame.id |= fonct << 3;     // Fonction appelée
+  frame.id |= prio << 27;       // Priorite 0, 1, 2 ou 3
+  frame.id |= cmde << 19;       // commande appelée
+  frame.id |= thisNodeId << 11; // ID expediteur
+  frame.id |= destNodeId << 3;  // ID destinataire
+  frame.id |= resp << 2;        // Réponse
   frame.ext = true;
   return frame;
 };
 
-void CanMsg::sendMsg(byte priorite, byte idExp, byte idDes, byte fonct)
+void CanMsg::sendMsg(byte prio, byte cmde, byte thisNodeId, byte destNodeId, byte resp)
 {
   CANMessage frame;
-  frame = parseMsg(frame, priorite, idExp, idDes, fonct);
+  frame = formatMsg(frame, prio, cmde, thisNodeId, destNodeId, resp);
   frame.len = 0;
   CanMsg::sendMsg(frame);
 }
 
-void CanMsg::sendMsg(byte priorite, byte idExp, byte idDes, byte fonct, byte data0)
+void CanMsg::sendMsg(byte prio, byte cmde, byte thisNodeId, byte destNodeId, byte resp, byte data0)
 {
   CANMessage frame;
-  frame = parseMsg(frame, priorite, idExp, idDes, fonct);
+  frame = formatMsg(frame, prio, cmde, thisNodeId, destNodeId, resp);
   frame.len = 1;
-  frame.data[0] = data0; // Fonction
+  frame.data[0] = data0;
   CanMsg::sendMsg(frame);
 }
 
-void CanMsg::sendMsg(byte priorite, byte idExp, byte idDes, byte fonct, byte data0, byte data1)
+void CanMsg::sendMsg(byte prio, byte cmde, byte thisNodeId, byte destNodeId, byte resp, byte data0, byte data1)
 {
   CANMessage frame;
-  frame = parseMsg(frame, priorite, idExp, idDes, fonct);
+  frame = formatMsg(frame, prio, cmde, thisNodeId, destNodeId, resp);
   frame.len = 2;
-  frame.data[0] = data0; // Fonction
+  frame.data[0] = data0;
   frame.data[1] = data1;
   CanMsg::sendMsg(frame);
 }
-
-void CanMsg::sendMsg(byte priorite, byte idExp, byte idDes, byte fonct, byte data0, byte data1, byte data2)
+void CanMsg::sendMsg(byte prio, byte cmde, byte thisNodeId, byte destNodeId, byte resp, byte data0, byte data1, byte data2)
 {
   CANMessage frame;
-  frame = parseMsg(frame, priorite, idExp, idDes, fonct);
+  frame = formatMsg(frame, prio, cmde, thisNodeId, destNodeId, resp);
   frame.len = 3;
-  frame.data[0] = data0; // Fonction
+  frame.data[0] = data0;
   frame.data[1] = data1;
   frame.data[2] = data2;
   CanMsg::sendMsg(frame);
 }
-
-void CanMsg::sendMsg(byte priorite, byte idExp, byte idDes, byte fonct, byte data0, byte data1, byte data2, byte data3)
+void CanMsg::sendMsg(byte prio, byte cmde, byte thisNodeId, byte destNodeId, byte resp, byte data0, byte data1, byte data2, byte data3)
 {
   CANMessage frame;
-  frame = parseMsg(frame, priorite, idExp, idDes, fonct);
+  frame = formatMsg(frame, prio, cmde, thisNodeId, destNodeId, resp);
   frame.len = 4;
-  frame.data[0] = data0; // Fonction
+  frame.data[0] = data0;
   frame.data[1] = data1;
   frame.data[2] = data2;
   frame.data[3] = data3;
-  CanMsg::sendMsg(frame);
-}
-
-void CanMsg::sendMsg(byte priorite, byte idExp, byte idDes, byte fonct, byte data0, byte data1, byte data2, byte data3, byte data4)
-{
-  CANMessage frame;
-  frame = parseMsg(frame, priorite, idExp, idDes, fonct);
-  frame.len = 5;
-  frame.data[0] = data0; // Fonction
-  frame.data[1] = data1;
-  frame.data[2] = data2;
-  frame.data[3] = data3;
-  frame.data[4] = data4;
-  CanMsg::sendMsg(frame);
-}
-
-void CanMsg::sendMsg(byte priorite, byte idExp, byte idDes, byte fonct, byte data0, byte data1, byte data2, byte data3, byte data4, byte data5)
-{
-  CANMessage frame;
-  frame = parseMsg(frame, priorite, idExp, idDes, fonct);
-  frame.len = 6;
-  frame.data[0] = data0; // Fonction
-  frame.data[1] = data1;
-  frame.data[2] = data2;
-  frame.data[3] = data3;
-  frame.data[4] = data4;
-  frame.data[5] = data5;
-  CanMsg::sendMsg(frame);
-}
-
-void CanMsg::sendMsg(byte priorite, byte idExp, byte idDes, byte fonct, byte data0, byte data1, byte data2, byte data3, byte data4, byte data5, byte data6)
-{
-  CANMessage frame;
-  frame = parseMsg(frame, priorite, idExp, idDes, fonct);
-  frame.len = 7;
-  frame.data[0] = data0; // Fonction
-  frame.data[1] = data1;
-  frame.data[2] = data2;
-  frame.data[3] = data3;
-  frame.data[4] = data4;
-  frame.data[5] = data5;
-  frame.data[6] = data6;
-  CanMsg::sendMsg(frame);
-}
-
-void CanMsg::sendMsg(byte priorite, byte idExp, byte idDes, byte fonct, byte data0, byte data1, byte data2, byte data3, byte data4, byte data5, byte data6, byte data7)
-{
-  CANMessage frame;
-  frame = parseMsg(frame, priorite, idExp, idDes, fonct);
-  frame.len = 8;
-  frame.data[0] = data0; // Fonction
-  frame.data[1] = data1;
-  frame.data[2] = data2;
-  frame.data[3] = data3;
-  frame.data[4] = data4;
-  frame.data[5] = data5;
-  frame.data[6] = data6;
-  frame.data[7] = data7;
   CanMsg::sendMsg(frame);
 }
